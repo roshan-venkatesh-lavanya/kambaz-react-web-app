410e5089a2cec9c77474b1187dcf1e5f1a9e5b20
roshan-venkatesh-lavanya <roshan.venla@gmail.com>
2025-06-18 02:46:01 +0530

    update

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,39 +1,39 @@
-
 import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
-import AssignmentEditor from "./Assignments/Editor";
+import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
+import AssignmentEditor from "./Assignments/editor";
+import People from "./People";
+import { FaAlignJustify } from "react-icons/fa";
 
+export default function Courses({ courses }: { courses: any[] }) {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
 
-import { Navigate, Route, Routes } from "react-router-dom";
-
-export default function Courses() {
   return (
     <div id="wd-courses">
-        <h2>Course 1234</h2>
-        
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-3 fs-4 mb-1" />
+        {course && course.name} &gt; {pathname.split("/")[4]}
+      </h2>
       <hr />
-      <table>
-        <tr>
-          <td valign="top">
-            <CourseNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/" element={<Navigate to="Home" />} />
-              <Route path="Home" element={<Home />} />
-              <Route path="Modules" element={<Modules />} />
-              <Route path="Assignments" element={<Assignments />} />
-              <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-              <Route path="Piazza" element={<h2>Piazza</h2>} />
-              <Route path="Grades" element={<h2>Grades</h2>} />
-              <Route path="Zoom" element={<h2>Zoom feature is not available</h2>} />
-              <Route path="Quizzes" element={<h2>Quizzes</h2>} />
-              <Route path="People" element={<h2>People</h2>} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
+      <div className="d-flex">
+        <div className="d-none d-md-block">
+          <CourseNavigation />
+        </div>
+        <div className="flex-fill">
+          <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
+            <Route path="Assignments" element={<Assignments />} />
+            <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+            <Route path="People" element={<People />} /> 
+          </Routes>
+        </div>
+      </div>
     </div>
-);}
+  );
+}
