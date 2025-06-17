1253b08794c1595e23f755ce9be665c15fc18f50
roshan-venkatesh-lavanya <roshan.venla@gmail.com>
2025-06-18 02:41:47 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import Kambaz from "./Kambaz";
 import Labs from "./Labs";
-import { Route, Navigate, Routes, HashRouter } from "react-router-dom";
+import Kambaz from "./Kambaz";
+import store from "./Kambaz/store";
+import { Provider } from "react-redux";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
- return (
-
-  <HashRouter>
-   <div>
-    <Routes>
-          <Route path="/" element={<Navigate to="Labs" />} />
-          <Route path="/Labs/*" element={<Labs />} />
-          <Route path="/Kambaz/*" element={<Kambaz />} />
-
-    </Routes>
-   </div>
-  </HashRouter>
-  
-);}
-
-
+  return (
+    <HashRouter>
+      <Provider store={store}>
+        <div>
+          <Routes>
+            <Route path="/" element={<Navigate to="Kambaz" />} />
+            <Route path="/Labs/*" element={<Labs />} />
+            <Route path="/Kambaz/*" element={<Kambaz />} />
+          </Routes>
+        </div>
+      </Provider>
+    </HashRouter>
+  );
+}
