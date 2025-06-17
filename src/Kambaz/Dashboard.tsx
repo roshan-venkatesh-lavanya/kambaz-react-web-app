189e5de2911fd974e20f4aab0080e1a8d9374ff7
roshan-venkatesh-lavanya <roshan.venla@gmail.com>
2025-06-18 02:44:12 +0530

    update

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,94 +1,192 @@
+import { Button, Card, Col, FormControl, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
-export default function Dashboard() {
+import { useSelector } from "react-redux";
+
+export default function Dashboard({
+  courses,
+  course,
+  setCourse,
+  addCourse,
+  deleteCourse,
+  updateCourse,
+  enrolling,
+  setEnrolling,
+  updateEnrollment,
+}: {
+  courses: any[];
+  course: any;
+  setCourse: (course: any) => void;
+  addCourse: () => void;
+  deleteCourse: (courseId: string) => void;
+  updateCourse: () => void;
+  enrolling: boolean;
+  setEnrolling: (enrolling: boolean) => void;
+  updateEnrollment: (courseId: string, enrolled: boolean) => void;
+}) {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const isFaculty = currentUser?.role === "FACULTY";
+  const defaultImageUrl = "/images/reactjs.jpg";
+
   return (
-    <div id="wd-dashboard">
-      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (7)</h2> <hr />
+    <div id="wd-dashboard" className="p-4">
+      <h1 id="wd-dashboard-title">Dashboard</h1>
+      <hr />
+
+      {isFaculty && (
+        <>
+          <h5>
+            Course Editor
+            <Button
+              className="btn btn-primary float-end ms-2"
+              id="wd-add-new-course-click"
+              onClick={addCourse}
+              disabled={!course.name || !course.description}
+            >
+              Add
+            </Button>
+            <Button
+              className="btn btn-warning float-end"
+              id="wd-update-course-click"
+              onClick={updateCourse}
+              disabled={!course._id}
+            >
+              Update
+            </Button>
+          </h5>
+
+          <FormControl
+            className="mb-2"
+            placeholder="Course Name"
+            value={course.name}
+            onChange={(e) => setCourse({ ...course, name: e.target.value })}
+          />
+          <FormControl
+            as="textarea"
+            className="mb-2"
+            placeholder="Course Description"
+            value={course.description}
+            onChange={(e) =>
+              setCourse({ ...course, description: e.target.value })
+            }
+            rows={3}
+          />
+          <hr />
+        </>
+      )}
+
+      <div className="d-flex justify-content-between align-items-center">
+        <h2 id="wd-dashboard-published">
+          Published Courses ({courses.length})
+        </h2>
+        {!isFaculty && (
+          <Button
+            className="btn btn-primary"
+            onClick={() => setEnrolling(!enrolling)}
+          >
+            {enrolling ? "Enrolled Courses" : "All Courses"}
+          </Button>
+        )}
+      </div>
+      <hr />
+
       <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS1234 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-      <div className="wd-dashboard-course">           
-       <Link to="/Kambaz/Courses/2345/Home"
-              className="wd-dashboard-course-link" >
-          <img src="/images/nodejs.jpg" width={200} />
-          <div>
-            <h5> CS2345   Node JS </h5>
-            <p className="wd-dashboard-course-title">
-              Full Stack software developer  </p>
-            <button> Go </button>
-          </div>
-       </Link>
+        <Row xs={1} md={5} className="g-4">
+          {courses.map((c) => {
+            const imageUrl = c.image || defaultImageUrl;
+            const isEnrolled = Boolean(c.enrolled);
+
+            return (
+              <Col
+                key={c._id}
+                className="wd-dashboard-course"
+                style={{ width: "350px" }}
+              >
+                <Card
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    height: "100%",
+                  }}
+                >
+                  <Link
+                    to={`/Kambaz/Courses/${c._id}/Home`}
+                    className="wd-dashboard-course-link text-decoration-none text-dark"
+                  >
+                    <Card.Img
+                      variant="top"
+                      src={imageUrl}
+                      height={200}
+                      alt={c.name}
+                    />
+                    <Card.Body style={{ flexGrow: 1 }}>
+                      <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                        {c.name}
+                      </Card.Title>
+                      <Card.Text
+                        className="wd-dashboard-course-description overflow-hidden"
+                        style={{ height: "100px" }}
+                      >
+                        {c.description}
+                      </Card.Text>
+                    </Card.Body>
+                  </Link>
+
+                  <Card.Body className="d-flex justify-content-between align-items-center">
+                    <Link to={`/Kambaz/Courses/${c._id}/Home`}>
+                      <Button variant="primary" size="sm">
+                        Go
+                      </Button>
+                    </Link>
+
+                    {isFaculty ? (
+                      <div>
+                        <Button
+                          variant="warning"
+                          size="sm"
+                          className="me-2"
+                          onClick={() => setCourse(c)}
+                          id="wd-edit-course-click"
+                        >
+                          Edit
+                        </Button>
+                        <Button
+                          variant="danger"
+                          size="sm"
+                          onClick={() => deleteCourse(c._id)}
+                          id="wd-delete-course-click"
+                        >
+                          Delete
+                        </Button>
+                      </div>
+                    ) : (
+                      enrolling && (
+                        <Button
+                          variant={isEnrolled ? "danger" : "success"}
+                          size="sm"
+                          onClick={(e) => {
+                            e.preventDefault();
+                            updateEnrollment(c._id, isEnrolled);
+                          }}
+                        >
+                          {isEnrolled ? "Unenroll" : "Enroll"}
+                        </Button>
+                      )
+                    )}
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+
+        {courses.length === 0 && (
+          <p className="text-muted mt-4">
+            {enrolling
+              ? "No courses available."
+              : "You are not enrolled in any courses."}
+          </p>
+        )}
       </div>
-      <div className="wd-dashboard-course">           
-       <Link to="/Kambaz/Courses/3456/Home"
-              className="wd-dashboard-course-link" >
-          <img src="/images/mongodb.jpg" width={200} />
-          <div>
-            <h5> CS3456   MongoDB </h5>
-            <p className="wd-dashboard-course-title">
-              Full Stack software developer  </p>
-            <button> Go </button>
-          </div>
-       </Link>
-      </div>
-        <div className="wd-dashboard-course">           
-         <Link to="/Kambaz/Courses/4567/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/expressjs.jpg" width={200} />
-            <div>
-                <h5> CS4567   Express JS </h5>
-                <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
-                <button> Go </button>
-            </div>
-         </Link>
-      </div>
-        <div className="wd-dashboard-course">           
-         <Link to="/Kambaz/Courses/5678/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/javascript.jpg" width={200} />
-            <div>
-                <h5> CS5678   JavaScript </h5>
-                <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
-                <button> Go </button>
-            </div>
-         </Link>
     </div>
-        <div className="wd-dashboard-course">           
-         <Link to="/Kambaz/Courses/6789/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/htmlcss.jpg" width={200} />
-            <div>
-                <h5> CS6789   HTML & CSS </h5>
-                <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
-                <button> Go </button>
-            </div>
-         </Link>
-    </div>
-        <div className="wd-dashboard-course">   
-            <Link to="/Kambaz/Courses/7890/Home"
-                    className="wd-dashboard-course-link" >
-                <img src="/images/bootstrap.jpg" width={200} />
-                <div>
-                    <h5> CS7890   Bootstrap </h5>
-                    <p className="wd-dashboard-course-title">
-                    Full Stack software developer  </p>
-                    <button> Go </button>
-                </div>
-                </Link>
-           </div>
-        </div>
-        </div>
-         );}
+  );
+}
--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,30 +1,150 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import Account from "./Account";
-import Dashboard from "./Dashboard";
-import KambazNavigation from "./Account/Navigation"; 
+import ProtectedRoute from "./Account/ProtectedRoute";
+import Session from "./Account/Session";
+import KambazNavigation from "./Navigation";
 import Courses from "./Courses";
-export default function Kambaz() {
-  return (
-    <div id="wd-kambaz">
-        <table>
-        <tr>
-          <td valign="top">
-            <KambazNavigation />
-          </td>
-          <td valign="top">
+import PeopleTable from "./Courses/People/Table";
+import Dashboard from "./Dashboard";
+import "./styles.css";
+import * as courseClient from "./Courses/client";
+import * as userClient from "./Account/client";
 
-      <Routes>
-        <Route path="/" element={<Navigate to="Account" />} />
-        <Route path="/Account/*" element={<Account />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-              <Route path="/Courses/:cid/*" element={<Courses />} />
-              <Route path="/Calendar" element={<h1>Calendar</h1>} />
-              <Route path="/Inbox" element={<h1>Inbox</h1>} />
-      </Routes>
-      
-        </td>
-        </tr>
-      </table>
-    </div>
+export default function Kambaz() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const [courses, setCourses] = useState<any[]>([]);
+  const [enrolling, setEnrolling] = useState<boolean>(false);
+
+  const [course, setCourse] = useState<any>({
+    _id: "1234",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    description: "New Description",
+  });
+
+  const findCoursesForUser = async () => {
+    try {
+      const userCourses = await userClient.findCoursesForUser(currentUser._id);
+      setCourses(userCourses);
+    } catch (error) {
+      console.error("Error fetching user courses:", error);
+    }
+  };
+
+  const fetchCourses = async () => {
+    try {
+      const allCourses = await courseClient.fetchAllCourses();
+      const enrolledCourses = await userClient.findCoursesForUser(
+        currentUser._id
+      );
+      const updatedCourses = allCourses.map((course: any) => {
+        const enrolled = enrolledCourses.find((c: any) => c._id === course._id);
+        return { ...course, enrolled: !!enrolled };
+      });
+      setCourses(updatedCourses);
+    } catch (error) {
+      console.error("Error fetching all courses:", error);
+    }
+  };
+
+  const addCourse = async () => {
+    try {
+      const newCourse = await courseClient.createCourse(course);
+      setCourses([...courses, newCourse]);
+    } catch (error) {
+      console.error("Error adding course:", error);
+    }
+  };
+
+  const deleteCourse = async (courseId: string) => {
+    try {
+      await courseClient.deleteCourse(courseId);
+      setCourses((prevCourses) =>
+        prevCourses.filter((course) => course._id !== courseId)
+      );
+    } catch (error) {
+      console.error("Error deleting course:", error);
+    }
+  };
+
+  const updateCourse = async () => {
+    try {
+      await courseClient.updateCourse(course);
+      setCourses((prevCourses) =>
+        prevCourses.map((c) => (c._id === course._id ? course : c))
+      );
+    } catch (error) {
+      console.error("Error updating course:", error);
+    }
+  };
+
+  const updateEnrollment = async (courseId: string, enrolled: boolean) => {
+    try {
+      if (enrolled) {
+        await userClient.enrollIntoCourse(currentUser._id, courseId);
+      } else {
+        await userClient.unenrollFromCourse(currentUser._id, courseId);
+      }
+
+      await fetchCourses();
+    } catch (error) {
+      console.error("Error updating enrollment:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (!currentUser) return;
+    if (enrolling) {
+      fetchCourses();
+    } else {
+      findCoursesForUser();
+    }
+  }, [currentUser, enrolling]);
+
+  return (
+    <Session>
+      <div id="wd-kambaz">
+        <KambazNavigation />
+        <div className="wd-main-content-offset p-3">
+          <Routes>
+            <Route path="/" element={<Navigate to="Account" />} />
+            <Route path="/Account/*" element={<Account />} />
+            <Route
+              path="/Dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard
+                    courses={courses}
+                    course={course}
+                    setCourse={setCourse}
+                    addCourse={addCourse}
+                    deleteCourse={deleteCourse}
+                    updateCourse={updateCourse}
+                    enrolling={enrolling}
+                    setEnrolling={setEnrolling}
+                    updateEnrollment={updateEnrollment}
+                  />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/Courses/:cid/*"
+              element={
+                <ProtectedRoute>
+                  <Courses courses={courses} />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/People" element={<PeopleTable />} />
+            <Route path="/Calendar" element={<h1>Calendar</h1>} />
+            <Route path="/Inbox" element={<h1>Inbox</h1>} />
+          </Routes>
+        </div>
+      </div>
+    </Session>
   );
 }
